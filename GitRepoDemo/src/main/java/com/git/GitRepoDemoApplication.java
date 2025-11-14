4018c300bc2f3a76277939b17b49200822a253f4
Prabhakaran G <prabhakaransaai@gmail.com>
2025-11-14 22:13:50 +0530

    Initial Commit

---
+++ GitRepoDemo/src/main/java/com/git/GitRepoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepoDemoApplication.class, args);
+	}
+
+}
