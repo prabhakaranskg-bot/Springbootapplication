8c7018f98050aa4844adee24a4801662a9d555d5
Prabhakaran G <prabhakaransaai@gmail.com>
2025-11-15 13:40:20 +0530

    dummy commit

--- GitRepoDemo/src/main/java/com/git/GitRepoDemoApplication.java
+++ GitRepoDemo/src/main/java/com/git/GitRepoDemoApplication.java
@@ -10,5 +10,5 @@ public class GitRepoDemoApplication {
 		SpringApplication.run(GitRepoDemoApplication.class, args);
 	}
 
-	//Dummy commit for git repo commit again
+	//Dummy commit for git repo commit
 }
