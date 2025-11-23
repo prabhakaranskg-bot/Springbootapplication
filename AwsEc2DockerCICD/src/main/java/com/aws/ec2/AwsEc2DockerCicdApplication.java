3479c217d41e95c15a03d25afd105ca4306cde69
Prabhakaran G <prabhakaransaai@gmail.com>
2025-11-23 11:56:44 +0530

    Initial Commit CICD

---
+++ AwsEc2DockerCICD/src/main/java/com/aws/ec2/AwsEc2DockerCicdApplication.java
@@ -0,0 +1,13 @@
+package com.aws.ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEc2DockerCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEc2DockerCicdApplication.class, args);
+	}
+
+}
